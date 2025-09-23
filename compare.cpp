fd63b4347c54700f5d11f4ce8a169e731c62b938
ASHFIQUL005 <543mdrajin@gmail.com>
2025-09-23 15:44:15 +0600

    compare.cpp updated

--- compare.cpp
+++ compare.cpp
@@ -7,6 +7,63 @@ using namespace std;
 double f(double x) {
     return exp(x) - 3*x;
 }
+
+/* ================= Modified False Position Method ================= */
+double modifiedFalsePosition(double x1, double x2, double tol, int maxIter, int &iterCount) {
+        if (f(x1) * f(x2) > 0) {
+        cout << "Wrong guesses! f(x1) and f(x2) must have opposite signs." << endl;
+        return NAN;
+    }
+
+    double x0 = x1;
+    double f0;
+    double f1 = f(x1), f2 = f(x2);
+
+    cout << "\n--- Modified False Position Method ---\n";
+    cout << left << setw(6) << "Iter"
+         << setw(12) << "x1"
+         << setw(12) << "x2"
+         << setw(12) << "x0"
+         << setw(12) << "f(x0)"
+         << setw(12) << "f(x1)"
+         << setw(12) << "f(x2)"
+         << setw(16) << "Relative Error" << endl;
+
+    for (iterCount = 1; iterCount <= maxIter; iterCount++) {
+        double x_new = (x1*f2 - x2*f1) / (f2 - f1);
+        f0 = f(x_new);
+        double rel_error = fabs(x_new - x0) / fabs(x_new);
+
+        cout << left << setw(6) << iterCount
+             << setw(12) << x1
+             << setw(12) << x2
+             << setw(12) << x_new
+             << setw(12) << f0
+             << setw(12) << f1
+             << setw(12) << f2
+             << setw(16) << rel_error << endl;
+
+        if (fabs(f0) < tol || rel_error < tol) {
+            cout << "\nRoot (Modified False Position) = " << x_new
+                 << " after " << iterCount << " iterations.\n";
+            return x_new;
+        }
+
+        if (f1 * f0 < 0) {
+            x2 = x_new;
+            f2 = f0 / 2;   // update trick
+        } else {
+            x1 = x_new;
+            f1 = f0 / 2;
+        }
+
+        x0 = x_new;
+    }
+
+    cout << "Max iterations reached in Modified False Position.\n";
+
+    return x0;
+}
 /* ================= Secant Method ================= */
 double secant(double x1, double x2, double tol, int maxIter, int &iterCount) {
         double f1 = f(x1), f2 = f(x2);
@@ -83,4 +140,5 @@ int main() {
 
     return 0;
 }
+
 
